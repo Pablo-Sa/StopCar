cfadad6adf1f60d028dcc1538412c2f9d659d306
Pablo Vinícius Pereira de Sá <56324757+Pablo-Sa@users.noreply.github.com>
2020-09-15 15:26:00 -0300

    Criado novos serviços

---
+++ src/app/model/CarsModel.ts
@@ -0,0 +1,7 @@
+export class CarsModel{
+    id:string;
+    modelo:string;
+    ano:string;
+    cor:string;
+    placa:string;
+}
--- src/app/shared/cars.service.ts
+++ src/app/shared/cars.service.ts
@@ -1,9 +1,23 @@
+import { CarsModel } from './../model/CarsModel';
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarsService {
 
-  constructor() { }
+  private clienteCollection: AngularFirestoreCollection<CarsModel> = this.db.collection("veiculos");
+
+  constructor(private db: AngularFirestore) {}
+
+  insert(cars: CarsModel) {
+    return this.clienteCollection.add({...cars});
+  }
+
+  getAll():Observable<CarsModel[]>{
+    return this.clienteCollection.valueChanges();
+  }
+
 }
